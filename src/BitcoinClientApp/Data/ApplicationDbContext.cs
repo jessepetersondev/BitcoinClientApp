f286091d022a1a072f7a28f37410862338731d20
Jesse Peterson <jesse.peterson.dev@proton.me>
2025-04-29 16:06:09 -0500

    Updated dbcontext and ran migration

--- src/BitcoinClientApp/Data/ApplicationDbContext.cs
+++ src/BitcoinClientApp/Data/ApplicationDbContext.cs
@@ -10,19 +10,34 @@ namespace BitcoinClientApp.Data
         {
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Wallet> Wallets => Set<Wallet>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<User>()
-                .HasIndex(u => u.Username)
-                .IsUnique();
+            // User configuration
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.HasIndex(e => e.Username).IsUnique();
+            });
 
-            modelBuilder.Entity<Wallet>()
-                .HasIndex(w => w.Username);
+            // Wallet configuration
+            modelBuilder.Entity<Wallet>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.EncryptedPrivateKey).IsRequired();
+                entity.Property(e => e.Salt).IsRequired();
+                entity.Property(e => e.PublicKey).IsRequired();
+                entity.Property(e => e.Address).IsRequired();
+                entity.HasIndex(e => e.Username);
+                entity.HasIndex(e => e.Address);
+            });
         }
     }
 } 
